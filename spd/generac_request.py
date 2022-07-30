b1f57e7e38dae7b77ddeeacd08de9ef2fd98664a
JesseLefty <jesselefty88@gmail.com>
2022-07-30 13:24:38 -0500

    add save_data global function and DashboardInfo.save_file local function

--- spd/generac_request.py
+++ spd/generac_request.py
@@ -1,11 +1,22 @@
 import requests
-
+import json
+import os
 
 DVCS_MAP = {
-            'NAME': 'n',
-            'IDS': 's',
-            'STATE': 'st'
-            }
+    'NAME': 'n',
+    'IDS': 's',
+    'STATE': 'st',
+    'POWER': 'p',
+    'TOTAL_ENERGY': 'et',
+    'UPDATE': 'up'
+}
+
+
+def save_data(data: dict, file_path: str, file_name: str):
+    output_json = json.dumps(data, indent=4)
+    path = os.path.join(file_path, file_name + '.json')
+    with open(path, 'w') as file:
+        file.write(output_json)
 
 
 class DashboardInfo:
@@ -22,6 +33,21 @@ class DashboardInfo:
         else:
             request_list = [str(i) for i in request_list]
             self.device_info[info] = [i[DVCS_MAP[info]] for i in self.devices['dvcs'] if
-                                       i[DVCS_MAP[search_criteria]] in request_list]
+                                      i[DVCS_MAP[search_criteria]] in request_list]
 
         return self.device_info[info]
+
+    def save_file(self, file_path: str, file_name: str):
+        save_data(self.devices, file_path, file_name)
+
+
+class Data:
+
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    def power(self):
+        pass
+
+    def energy(self):
+        pass
