58105193dc9683e2df87f9989f0a3b8b97794a7f
JesseLefty <jesselefty88@gmail.com>
2022-07-30 00:24:54 -0500

    quick refactor because turns out I was doing things the hard way the first time

--- spd/generac_request.py
+++ spd/generac_request.py
@@ -14,31 +14,14 @@ class DashboardInfo:
         self.url = url
         self.headers = headers
         self.devices = requests.get(self.url, headers=self.headers).json()
-        self.device_ids = []
-        self.device_names = []
-        self.device_state = []
-
-    def _clear(self):
-        self.device_ids = []
-        self.device_names = []
-        self.device_state = []
-        self.device_info = {
-                            'IDS': self.device_ids,
-                            'NAME': self.device_names,
-                            'STATE': self.device_state
-                            }
+        self.device_info = {}
 
     def device_information(self, info, search_criteria=None, request_list=None):
-        self._clear()
         if request_list is None and search_criteria is None:
-            self.device_info['IDS'] = [i[DVCS_MAP['IDS']] for i in self.devices['dvcs']]
-            self.device_info['NAME'] = [i[DVCS_MAP['NAME']] for i in self.devices['dvcs']]
-            self.device_info['STATE'] = [i[DVCS_MAP['STATE']] for i in self.devices['dvcs']]
+            self.device_info[info] = [i[DVCS_MAP[info]] for i in self.devices['dvcs']]
         else:
             request_list = [str(i) for i in request_list]
-            for item in self.devices['dvcs']:
-                if item[DVCS_MAP[search_criteria]] in request_list:
-                    self.device_info['IDS'].append(item[DVCS_MAP['IDS']])
-                    self.device_info['NAME'].append(item[DVCS_MAP['NAME']])
-                    self.device_info['STATE'].append(item[DVCS_MAP['STATE']])
+            self.device_info[info] = [i[DVCS_MAP[info]] for i in self.devices['dvcs'] if
+                                       i[DVCS_MAP[search_criteria]] in request_list]
+
         return self.device_info[info]
