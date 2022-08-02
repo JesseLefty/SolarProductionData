e29a0214ca889f267a26943a1805419023548ebc
JesseLefty <jesselefty88@gmail.com>
2022-08-02 06:25:21 -0500

    updated min and max functions and began work on 'all_data' function

--- spd/data_functions.py
+++ spd/data_functions.py
@@ -7,7 +7,13 @@ def date_to_epoch(date: str, hour: int = 0):
     pattern = '%m-%d-%Y: %H'
     return int(time.mktime(time.strptime(complete_date, pattern)))
 
-#todo: provide corresponding date and time with max / min
+
+def epoch_to_date(date: int):
+    return time.strftime("%d %b %Y %H:%M:%S", time.gmtime(date))
+
+
+
+
 #todo: figure out how class inheritance works
 
 
@@ -29,28 +35,57 @@ class ProductionData:
         else:
             self.end_time = date_to_epoch(end_date, end_hour)
         self.zero_list = [None]
-        self.data_in_range = self.production_in_range()
+        self.data_in_range = self._production_in_range()
+        self.time = [i[0] for i in self.data_in_range]
+        self.production = [i[1] for i in self.data_in_range]
+        self.bool = [i[2] for i in self.data_in_range]
 
     def count_zero(self):
-        return len([i for i in self.data_in_range if i[1] <= 0])
+        return len([i for i in self.production if i <= 0])
 
     def get_zero(self):
         self.zero_list = [i for i in self.data_in_range if i[1] >= 0]
         return self.zero_list
 
-    def production_in_range(self):
-        return [i for i in self.data if self.begin_time <= i[0]/1000 < self.end_time]
+    def _production_in_range(self):
+        return [i for i in self.data if self.begin_time*1000 <= i[0] < self.end_time*1000]
 
-    def max(self):
-        return max([i[1] for i in self.data_in_range])
+    def max(self, with_date=False, epoch=True):
+        max_val = max([i for i in self.production])
+        if not with_date:
+            return max_val
+        else:
+            max_index = self.production.index(max_val)
+            date = self.time[max_index]
+            if not epoch:
+                date = epoch_to_date(date)
+            return [date, max_val]
+#todo: fix the epoch_to_date conversion
 
-    def min(self):
-        return min([i[1] for i in self.data_in_range])
+    def min(self, with_date=False, epoch=True):
+        min_val = min([i for i in self.production])
+        if not with_date:
+            return min_val
+        else:
+            min_index = self.production.index(min_val)
+            date = self.time[min_index]
+            if not epoch:
+                date = epoch_to_date(date)
+            return [date, min_val]
 
-    def all_data(self, zeros=False):
-        pass
+    def all_data(self, zeros=False, epoch=True):
+        data_dict = {'Range': [self.begin_time, self.end_time],
+                     'Max': self.max(with_date=True, epoch=epoch),
+                     'Min': self.min(with_date=True, epoch=epoch),
+                     'No Production': self.count_zero()}
+        if not epoch:
+            data_dict['Range'] = [epoch_to_date(self.begin_time), epoch_to_date(self.end_time)]
+        return data_dict
 
     def daily(self, zeros=False):
+        pass
+
+    def match_date(self, value: int):
         pass
 
 
@@ -59,9 +94,10 @@ class Power(ProductionData):
         super().__init__(data)
 
 
-print(date_to_epoch('07-20-2022'))
 with open(r'C:\Users\Jesse\Desktop\Solar Data\power test.json', 'r') as f:
     data = json.load(f)
 power_data = data['wh_per_hour']
 
 power = ProductionData(power_data, begin_date='05-10-2021', end_date='7-12-2022')
+print(power.max())
+print(power.all_data(epoch=False))
