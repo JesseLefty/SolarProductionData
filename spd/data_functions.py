c09019bf0bc07b01aff95b21769127f847775004
JesseLefty <jesselefty88@gmail.com>
2022-08-06 22:16:08 -0500

    finished the framework for the Power class. Majority of methods are identified, doc strings and profiling still required.

--- spd/data_functions.py
+++ spd/data_functions.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 import time
-import json
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
@@ -9,19 +8,22 @@ def date_to_epoch(date: str):
     return int(time.mktime(time.strptime(date, DATE_FORMAT)))
 
 
-class ProductionData:
+class Power:
 
     def __init__(self, data: list[list],
-                 begin_date: str = '',
-                 end_date: str = ''):
-
+                 begin_date: str,
+                 end_date: str):
         self.data = data
+        self.time = [datetime.fromtimestamp(t[0] // 1000).strftime(DATE_FORMAT) for t in self.data]
         self.begin_time = begin_date
         self.end_time = end_date
-        self.zero_list = [None]
-        self.time = [datetime.fromtimestamp(t[0] // 1000).strftime(DATE_FORMAT) for t in self.data]
+
+        if not self._check_valid_dates():
+            raise ValueError
+
         self.production = [i[1] for i in self.data]
         self.bool = [i[2] for i in self.data]
+        self.zero_list = [None]
         self.data_in_range = self._production_in_range()
         self.time_in_range = [datetime.fromtimestamp(t[0] // 1000).strftime(DATE_FORMAT) for t in self.data_in_range]
         self.production_in_range = [i[1] for i in self.data_in_range]
@@ -29,17 +31,41 @@ class ProductionData:
         self.start_date_list = self.begin_time.split('-')
         self.end_date_list = self.end_time.split('-')
 
+    def _check_valid_dates(self):
+        if self.time[0] > self.begin_time:
+            print(f'begin date cannot be earlier than {self.time[0]}')
+            return False
+        elif self.time[-1] < self.end_time:
+            print(f'end date cannot be later than {self.time[-1]}')
+        elif self.end_time <= self.begin_time:
+            print(f'end date cannot be before or equal to the begin date')
+        else:
+            return True
+
+    def _production_in_range(self):
+        begin_date = date_to_epoch(self.begin_time)
+        end_date = date_to_epoch(self.end_time)
+        return [item for idx, item in enumerate(self.data) if begin_date <= date_to_epoch(self.time[idx]) <= end_date]
+
+    @staticmethod
+    def _average(power_data: list, date_data: list, n: int = 1, precision: int = 2):
+        i = 0
+        result = {}
+        while i < len(power_data) - n + 1:
+            power_window = power_data[i:i + n]
+            date_window = date_data[i:i + n]
+            window_average = round(sum(power_window) / n, precision)
+            result[(date_window[0], date_window[-1])] = window_average
+            i += 1
+
+        return result
+
     def count_zero(self):
         return len([i for i in self.production if i <= 0])
 
     def get_zero(self):
         self.zero_list = [i for i in self.data_in_range if i[1] >= 0]
         return self.zero_list
-
-    def _production_in_range(self):
-        begin_date = date_to_epoch(self.begin_time)
-        end_date = date_to_epoch(self.end_time)
-        return [item for idx, item in enumerate(self.data) if begin_date <= date_to_epoch(self.time[idx]) <= end_date]
 
     def max(self, with_date=False):
         max_val = max([i for i in self.production_in_range])
@@ -59,9 +85,28 @@ class ProductionData:
             date = self.time_in_range[min_index]
             return [date, min_val]
 
+    def hourly_average(self, n: int = 1, precision: int = 1):
+        return self._average(self.production_in_range, self.data_in_range, n=n, precision=precision)
+
+    def daily_average(self, n: int = 1, precision: int = 1):
+        daily_data = self.group_by_day()
+        daily_production = [sum(v) for v in daily_data.values()]
+        daily_dates = [k for k in daily_data.keys()]
+        return self._average(daily_production, daily_dates, n=n, precision=precision)
+
+    def weekly_average(self):
+        pass
+
+    def monthly_average(self):
+        pass
+
+    def yearly_average(self):
+        pass
+
     def all_data(self):
         data_dict = {'Range': [self.begin_time, self.end_time],
-                     'Max': self.max(with_date=True),
+                     'Max': {'Hour': self.max(with_date=True),
+                             'Day': 'WIP'},
                      'Min': self.min(with_date=True),
                      'No Production': self.count_zero()}
         return data_dict
@@ -85,20 +130,11 @@ class ProductionData:
             days[datetime.strftime(date, DATE_FORMAT)] = day_list
         return days
 
-    def match_date(self, value: int):
+    def group_by_week(self):
         pass
 
+    def group_by_month(self):
+        pass
 
-class Power(ProductionData):
-    def __init__(self, data: list[list]):
-        super().__init__(data)
-
-
-with open(r'C:\Users\Jesse\Desktop\Solar Data\power test.json', 'r') as f:
-    data = json.load(f)
-power_data = data['wh_per_hour']
-
-power = ProductionData(power_data, begin_date='2022-06-10 20:00:00', end_date='2022-07-12 10:00:00')
-print(power.group_by_day())
-
-
+    def group_by_year(self):
+        pass
